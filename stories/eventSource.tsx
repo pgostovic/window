fef889e015889abec314f2b3fb329930e2b2bd9c
Patrick Gostovic <pg@patricks-mbp.lan>
2020-03-22 10:30:00 -0400

    Added some instructions.

--- stories/eventSource.tsx
+++ stories/eventSource.tsx
@@ -8,19 +8,23 @@ for (let i = 0; i < 1000; i++) {
 }
 
 export const EventSource: FC = () => (
-  <Window eventSource={window} style={{ height: '500px', width: '200px' }} items={numbers}>
-    {(num: number) => (
-      <div style={{ borderBottom: '1px solid #ccc', display: 'flex', alignItems: 'center' }}>
-        {num}
-      </div>
-    )}
-  </Window>
+  <>
+    Scroll with the cursor anywhere in the frame.
+    <Window eventSource={window} style={{ height: '500px', width: '200px' }} items={numbers}>
+      {(num: number) => (
+        <div style={{ borderBottom: '1px solid #ccc', display: 'flex', alignItems: 'center' }}>
+          {num}
+        </div>
+      )}
+    </Window>
+  </>
 );
 
 export const EventSourceRef: FC = () => {
   const rootRef = createRef<HTMLDivElement>();
   return (
     <div ref={rootRef} style={{ height: '700px', width: '400px', backgroundColor: '#eee' }}>
+      Scroll with the cursor anywhere in the shaded region.
       <Window eventSourceRef={rootRef} style={{ height: '500px', width: '200px' }} items={numbers}>
         {(num: number) => (
           <div style={{ borderBottom: '1px solid #ccc', display: 'flex', alignItems: 'center' }}>
