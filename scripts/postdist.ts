e1b796f92bac68d5a225c8ee3a4eb8ffbb90bfa0
Patrick Gostovic <pg@phranq.com>
2021-11-22 12:18:29 -0500

    Keep it basic.

--- scripts/postdist.ts
+++ scripts/postdist.ts
@@ -3,30 +3,6 @@ import path from 'path';
 
 const pkg = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../package.json')).toString());
 
-const buildDeps = [
-  '@rollup/plugin-commonjs',
-  '@rollup/plugin-node-resolve',
-  '@rollup/plugin-replace',
-  '@types/node',
-  '@types/react',
-  '@types/react-dom',
-  '@types/styled-components',
-  'react',
-  'react-dom',
-  'rollup',
-  'rollup-plugin-peer-deps-external',
-  'rollup-plugin-terser',
-  'rollup-plugin-typescript2',
-  'ts-node',
-  'typescript',
-];
-
-const pruneDeps = (deps: { [key: string]: string }) =>
-  Object.keys(deps).reduce(
-    (depsPruned, key) => (buildDeps.includes(key) ? depsPruned : { ...depsPruned, [key]: deps[key] }),
-    {},
-  );
-
 const {
   author,
   browser,
@@ -36,8 +12,6 @@ const {
   engines,
   keywords,
   license,
-  main,
-  module: pkgModule,
   name: packageName,
   repository,
   version,
@@ -46,15 +20,15 @@ const {
 const distPkgJSON = {
   author,
   browser,
-  dependencies: pruneDeps(dependencies),
+  dependencies,
   peerDependencies,
   description,
   engines,
   keywords,
   license,
-  main: path.basename(main),
-  module: path.basename(pkgModule),
-  'jsnext:main': path.basename(pkg['jsnext:main']),
+  main: 'index.js',
+  module: 'index.es.js',
+  'jsnext:main': 'index.es.js',
   name: packageName,
   repository,
   version,
